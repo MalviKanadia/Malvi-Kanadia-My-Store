ba95957278f91e0f720f0cf4164e2526d7d1becf
MalviKanadia <W0849019@myscc.ca>
2024-09-18 17:20:47 -0400

    Assignment 2 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,11 @@ import {JsonPipe, NgForOf} from "@angular/common";
 })
 export class AppComponent {
   title = 'MyStore';
-  user1 : User ={id: 1,productName:"waterBottle",quantity:1,color:"cyan",giftWrap:false,};
-  user2 : User ={id: 2,productName:"bag",quantity:1,color:"black",giftWrap:false,};
+  user1 : User ={id: 1,productName:"WaterBottle",quantity:1,color:"cyan",giftWrap:false,};
+  user2 : User ={id: 2,productName:"Bag",quantity:1,color:"black",giftWrap:false,};
   userList: User[] =[this.user1, this.user2,
-    {id:3,productName:"lunchBox",quantity:2,color:"biege",giftWrap:true},
-    {id:4,productName:"box",quantity:4,color:"purple",giftWrap:true}]
+    {id:3,productName:"LunchBox",quantity:2,color:"biege",giftWrap:true},
+    {id:4,productName:"Box",quantity:4,color:"purple",giftWrap:true}]
 
   toggleGiftWrapStatus(user: User): void {
     user.giftWrap = !user.giftWrap;
