32045cb62e605436ecd46737d7cbd4e45a970068
MalviKanadia <W0849019@myscc.ca>
2024-09-18 16:43:19 -0400

    created five new properties and made one of them optional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from "./Shared/Modules/user";
+import {JsonPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgForOf,JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyStore';
+  user1 : User ={id: 1,productName:"waterBottle",quantity:1,color:"cyan",giftWrap:false,};
+  user2 : User ={id: 2,productName:"bag",quantity:1,color:"black",giftWrap:false,};
+  userList: User[] =[this.user1, this.user2,
+    {id:3,productName:"lunchBox",quantity:2,color:"biege",giftWrap:true},
+    {id:4,productName:"box",quantity:4,color:"purple",giftWrap:true}]
+
+  toggleGiftWrapStatus(user: User) void {
+    user.giftWrap = !user.giftWrap;
+  }
 }
