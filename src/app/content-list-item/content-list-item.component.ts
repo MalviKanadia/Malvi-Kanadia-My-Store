c4cfe22c67c8eb211f8ce33c58ff7eb06f0fc43a
MalviKanadia <W0849019@myscc.ca>
2024-09-19 17:44:50 -0400

    working on content-list-item.component and content-list.component files

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
 
+import {Component,Input} from '@angular/core';
+import {User} from "../Shared/Modules/user";
+import {NgIf} from "@angular/common";
 @Component({
   selector: 'app-content-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+  ],
   templateUrl: './content-list-item.component.html',
   styleUrl: './content-list-item.component.css'
 })
 export class ContentListItemComponent {
+  @Input() item?: User;
 
 }
