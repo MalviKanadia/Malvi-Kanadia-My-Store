09dd4f12f5d697a573dbddf3e7fb4720810f9918
MalviKanadia <W0849019@myscc.ca>
2024-09-19 17:23:10 -0400

    working on content-list.component.ts

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {User} from "../Shared/Modules/user";
+import {NgFor} from "@angular/common";
+import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgFor,
+    ContentListItemComponent,
+  ],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.css'
 })
 export class ContentListComponent {
+
+
+  userList: User[] =[
+    {id: 1, productName:"WaterBottle", quantity:1, color:"cyan", giftWrap:false,},
+    {id: 2, productName:"Bag", quantity:1, color:"black", giftWrap:false,},
+    {id:3, productName:"LunchBox", quantity:2, color:"biege", giftWrap:true},
+    {id:4,productName:"Box", quantity:4, color:"purple", giftWrap:true}];
+
+    selectedItem?: User;
+
+    selectItem(item: User):void{
+      this.selectedItem = item;
+    }
+
 
 }
